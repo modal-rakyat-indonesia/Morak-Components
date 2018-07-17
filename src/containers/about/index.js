80856175c079e54f9e0e84f9b97ce6d0047c9f7c
theodorusyoga <theoyoga@outlook.com>
2018-07-17 14:12:37 +0700

    [DEV-32] Added React Helmet and tested on about page.

--- src/containers/about/index.js
+++ src/containers/about/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
 export default () => (
   <div>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>
+        About Us
+      </title>
+      <meta name="description" content="This is description of the website" />
+    </Helmet>
     <h1>
       About Us
     </h1>
-    <p>
-      Hello Medium!
-    </p>
   </div>
 );
