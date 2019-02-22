bd4c662949a6290d4b48e1f0e399de8e44a9d8b4
Theodorus Yoga <theoyoga@outlook.com>
2019-02-22 11:35:12 +0700

    Changed fetch to axios get. Added example jpg with CORS

--- src/helpers/zip.js
+++ src/helpers/zip.js
@@ -1,27 +1,24 @@
 // import { external } from 'jszip';
-// import axios from 'axios';
+import axios from 'axios';
 import JSZip from 'jszip';
 import { saveAs } from 'file-saver';
 
-const exampleUrl = 'https://cors-anywhere.herokuapp.com/https://develop-modalrakyat.s3.amazonaws.com/documents/073e4540-2b6a-11e9-8d39-0ba82f87925c.jpg';
+const exampleUrl = 'https://develop-modalrakyat.s3.amazonaws.com/documents/cc7cfa90-365a-11e9-a862-5b18735e1091.jpg';
 // const exampleUrl = '/img/zip-test.jpg';
 
 export const getFilesFromUrls = (urls) => {
-  fetch(exampleUrl).then((response) => {
-    if (response.status === 200 || response.status === 0) {
-      return Promise.resolve(response.blob());
-    }
-    return Promise.reject();
-  })
-    .then((zip) => {
-      const zipfile = new JSZip();
-      zipfile.file('1.jpg', zip);
-      zipfile.file('2.jpg', zip);
-      zipfile.file('3.jpg', zip);
-      zipfile.generateAsync({ type: 'blob' }).then((blob) => {
-        saveAs(blob, 'result.zip');
-      });
+  axios.get(exampleUrl, {
+    responseType: 'blob'
+  }).then((response) => {
+    const blob = new Blob([response.data]);
+    const zipfile = new JSZip();
+    zipfile.file('test1.pdf', blob);
+    zipfile.file('test2.pdf', blob);
+    zipfile.file('test3.pdf', blob);
+    zipfile.generateAsync({ type: 'blob' }).then((blob) => {
+      saveAs(blob, 'result.zip');
     });
+  });
 };
 
 export const returnZipFile = (files) => {
